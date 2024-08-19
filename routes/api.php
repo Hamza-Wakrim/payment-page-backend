9d742a8fa6b79ddd855e3e4ccdc4a6d396591d0a
Wakrim Hamza <wakrimhamza90@gmail.com>
2024-08-19 14:10:33 +0100

    remove code

--- routes/api.php
+++ routes/api.php
@@ -23,35 +23,6 @@ Route::get('authZoho', function() {
     return 'ok';
 });
 
-Route::get('authZoho2', function() {
-    $response = Http::asForm()->post('https://accounts.zoho.com/oauth/v2/token', [
-        'code' => '1000.7d26e26e1ae65b517d2fd758635b7941.df336066ac74e2246f75aeff12872bd0',
-        'client_id' => '1000.4QCO0132SG66R8N3R113FO2VFJBB8Q',
-        'client_secret' => '27978a1b5eb1897aba48b7b94f972a7b44777ebafb',
-        'redirect_uri' => 'http://127.0.0.1:8000/api/authZoho',
-        'grant_type' => 'authorization_code',
-    ]);
-
-    // To get the response body as an array
-    $responseBody = $response->json();
-
-    // If you need to check the status code or handle the response differently
-    if ($response->successful()) {
-        // Log::info('reponse zoho: '. $responseBody);
-        $config = Config::updateOrCreate([
-            Config::ORGANIZATION_ID => '860147291',
-        ],[
-            Config::ORGANIZATION_ID => '860147291',
-            Config::DATA => json_decode(json_encode($responseBody))
-        ]);
-        dd('worked', $config, $responseBody);
-    } else {
-        dd($responseBody, $response->status());
-        // Handle error
-        // $response->status() will give you the HTTP status code
-    }
-});
-
 Route::get('/products', GetProductsController::class);
 Route::post('/customer', CreateCustomerController::class);
 Route::post('/hostedpages' , CreateHostedPageController::class);
